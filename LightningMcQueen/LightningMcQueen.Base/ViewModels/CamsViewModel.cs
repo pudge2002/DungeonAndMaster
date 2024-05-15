0bd29c32fcf1aadb6d4a9d973fe8e20b839881df
miIlMAX <mill29MAX@gmail.com>
2024-05-15 21:44:18 +0300

    Добавлено видео

--- LightningMcQueen/LightningMcQueen.Base/ViewModels/CamsViewModel.cs
+++ LightningMcQueen/LightningMcQueen.Base/ViewModels/CamsViewModel.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using Prism.Commands;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace LightningMcQueen.ViewModels;
 
-internal class CamsViewModel
+internal class CamsViewModel : ReactiveObject
 {
+
+    [Reactive] public string VideoPath { get; set; } = "..\\..\\..\\..\\..\\Video\\Aglomachine.mkv";
+
 }
