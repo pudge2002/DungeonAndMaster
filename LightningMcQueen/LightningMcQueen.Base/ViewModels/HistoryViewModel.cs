c20d4e1c2a31ad6b84d2d696a016b99efa0f6350
LoginOFFM <loginoffm@yandex.ru>
2024-05-20 00:18:03 +0300

    Create HistoryViewModel.cs
    added open json file

---
+++ LightningMcQueen/LightningMcQueen.Base/ViewModels/HistoryViewModel.cs
@@ -0,0 +1,52 @@
+﻿using DynamicData.Binding;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using Model;
+using Newtonsoft.Json;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Reactive;
+using System.Reflection;
+using System.Text;
+
+
+namespace LightningMcQueen.ViewModels
+{
+    internal class HistoryViewModel : ReactiveObject
+    {
+        public ObservableCollection<State> ShowableStates { get; set; }=new ObservableCollection<State>();
+        public ReactiveCommand<Unit, Unit> OpenFileCommand { get; private set; }
+        public HistoryViewModel()
+        {
+            OpenFileCommand = ReactiveCommand.Create(() => OpenFile());
+        }
+        public void OpenFile()
+        {
+           
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+
+            // Set the properties of the OpenFileDialog
+            openFileDialog.InitialDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "\\States"; ;
+            openFileDialog.Filter = "JSON files (*.json)|*.json";
+            openFileDialog.RestoreDirectory = true;
+
+            // Show the OpenFileDialog and check if the user selected a file
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // Get the path of the selected file
+                string filePath = openFileDialog.FileName;
+
+                // Read the contents of the file
+                string json =File.ReadAllText(filePath);
+
+
+                // Deserialize the JSON string into a list of states
+                ShowableStates = new ObservableCollection<State>(JsonConvert.DeserializeObject<ObservableCollection<State>>(json));
+
+                ShowableStates.Add(new State { Id = ShowableStates.Count, dateTime = DateTime.Now, Capital = "capital" });
+            }
+        }
+    }
+}
